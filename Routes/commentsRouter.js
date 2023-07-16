d43f4804362ef9681e23b1f9babae98a56aa26aa
Hadeel-Ayman <haddelayman4@gmail.com>
2023-07-16 05:55:01 +0300

    add changes

--- Routes/commentsRouter.js
+++ Routes/commentsRouter.js
@@ -1,8 +1,8 @@
 const express = require("express")
 const Comment = require('../Controllers/commentsController')
-
-const auth = require("../middleware/auth");
 const router = express.Router()
+const auth = require("../middleware/auth");
+
 router.post("/comment/:mentorApplicationId", auth, Comment.addComment);
 router.delete("/comment/:commentId", auth, Comment.deleteComment);
 router.get("/comment/:mentorApplicationId", Comment.getComment);
